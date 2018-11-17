c05ced4108ab0bfe5c58dff32ef9fda5a68ca64a
王嘉巍 <906789088@qq.com>
2018-11-17 17:09:06 +0800

    feat:using Iconfont

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -5,6 +5,7 @@ import {
   Logo,
   NAV,
   NavItem,
+  SearchWrapper,
   NavSearch,
   Addition,
   Button
@@ -18,11 +19,19 @@ class Header extends Component {
           <NavItem className='left active'>首页</NavItem>
           <NavItem className='left'>下载App</NavItem>
           <NavItem className='right'>登录</NavItem>
-          <NavItem className='right'>Aa</NavItem>
-          <NavSearch></NavSearch>
+          <NavItem className='right'>
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <NavSearch></NavSearch>
+            <i className="iconfont">&#xe614;</i>
+          </SearchWrapper>
         </NAV>
         <Addition>
-          <Button className='writting'>写文章</Button>
+          <Button className='writting'>
+            <i className="iconfont">&#xe615;</i>
+            写文章
+          </Button>
           <Button className='reg'>注册</Button>
         </Addition>
       </HeaderWrapper>
