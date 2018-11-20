996541e34e016596d0c4eff11ee8782d04ca0b6c
王嘉巍 <906789088@qq.com>
2018-11-20 17:51:38 +0800

    feat:using immutable.js manage store data

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -43,7 +43,7 @@ const Header = (props) => {
 }
 const mapStateToProps = (state) => {
   return {
-    focused:state.header.focused
+    focused:state.header.get('focused')
   }
 }
 const mapDispathToProps = (dispatch) => {
