fbd9a30231e28cbc096436e7a2dd103e5fd676e9
王嘉巍 <906789088@qq.com>
2018-11-21 22:53:37 +0800

    featAjax get data

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -6,8 +6,8 @@ import {  HeaderWrapper,Logo,NAV,NavItem,SearchWrapper, NavSearch,SearchInfo,Sea
 
 class Header extends Component {
 
-  getListArea(show) {
-    if (show) {
+  getListArea() {
+    if (this.props.focused) {
       return (
         <SearchInfo>
           <SearchInfoTitle>
@@ -15,12 +15,11 @@ class Header extends Component {
             <SearchInfoSwitch>换一批</SearchInfoSwitch>
           </SearchInfoTitle>
           <SearchInfoList>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
+            {
+              this.props.list.map((item) => {
+                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+              })
+            } 
           </SearchInfoList>
         </SearchInfo>
       )
@@ -54,7 +53,7 @@ class Header extends Component {
             <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>
               s
             </i>
-            {this.getListArea(this.props.focused)}
+            {this.getListArea()}
           </SearchWrapper>
         </NAV>
         <Addition>
@@ -71,7 +70,8 @@ class Header extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    focused:state.getIn(['header','focused'])
+    focused:  state.getIn(['header','focused']),
+    list: state.getIn(['header','list'])
   }
 }
 const mapDispathToProps = (dispatch) => {
