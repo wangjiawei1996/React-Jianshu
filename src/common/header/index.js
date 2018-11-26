6e307487c397d7c95de27579e370d6ef215f359f
王嘉巍 <906789088@qq.com>
2018-11-27 00:09:32 +0800

    feat:update this header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -40,7 +40,7 @@ class Header extends Component {
     }
   }
   render() {
-    const { focused, handleInputFocus, handleInputBlur } = this.props;
+    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
     return (
       <HeaderWrapper>
         <Logo href='/'/>
@@ -59,7 +59,7 @@ class Header extends Component {
             >
               <NavSearch
                 className={focused ? 'focused' : ''}
-                onFocus = {handleInputFocus}
+                onFocus = {() => handleInputFocus(list)}
                 onBlur = {handleInputBlur}
               ></NavSearch>
             </CSSTransition>
@@ -92,8 +92,8 @@ const mapStateToProps = (state) => {
 }
 const mapDispathToProps = (dispatch) => {
   return {
-    handleInputFocus() {
-      dispatch(actionCreators.getList());
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
