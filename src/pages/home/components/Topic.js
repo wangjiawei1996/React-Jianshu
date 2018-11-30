d63812696ebedc81b762e6ba46b9a5cf574827e3
王嘉巍 <906789088@qq.com>
2018-11-30 15:44:28 +0800

    feat:design topic reducer

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,18 +1,27 @@
-import React, { Component} from 'react'
-import { TopicWrapper,TopicItem } from '../style'
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import { TopicWrapper,TopicItem } from '../style';
 class Topic extends Component {
   render() {
+    const { list } = this.props
     return (
       <TopicWrapper>
-        <TopicItem>
-          <img
-            className='topic-pic '
-            src = '//upload.jianshu.io/collections/images/95/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp' 
-          />
-          社会热点
-        </TopicItem>
+        {
+          list.map((item) => (
+              <TopicItem key={item.get('id')}>
+                <img
+                  className='topic-pic '
+                  src = {item.get('imgUrl')}
+                />
+                {item.get('title')}
+              </TopicItem>
+            ))
+        } 
       </TopicWrapper>
     )
   }
 }
-export default Topic;
+const mapState = (state) => ({
+  list: state.get('home').get('topicList')
+});
+export default connect(mapState, null)(Topic);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,16 +1,19 @@
 import { fromJS } from 'immutable';
 
 const defaultState = fromJS({
-    focused: false,
-    mouseIn: false,
-    list:[],
-    page: 1,
-    totalPage: 1
-  });
+  topicList: [{
+    id: 1,
+    title: '社会热点',
+    imgUrl: '//upload.jianshu.io/collections/images/95/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp'
+  }, {
+    id: 2,
+    title: '手绘',
+    imgUrl: '//upload.jianshu.io/collections/images/283250/%E6%BC%AB%E7%94%BB%E4%B8%93%E9%A2%98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp'
+  }]
+});
   
   export default (state = defaultState,action) => {
     switch(action.type) {
-      
       default:
         return state;
     }
