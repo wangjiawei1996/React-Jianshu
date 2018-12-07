5b4f60b3f098b256009caa0effe931a40a2bf736
王嘉巍 <906789088@qq.com>
2018-12-08 00:32:32 +0800

    feat: routing-detail

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -23,8 +23,8 @@ const mapState = (state) => ({
   content: state.getIn(['detail','content'])
 });
 const mapDispatch = (dispatch) =>({
-  getDetail() {
-    dispatch(actionCreators.getDetail());
+  getDetail(id) {
+    dispatch(actionCreators.getDetail(id));
   }
 })
 export default connect(mapState,mapDispatch)(Detail);
